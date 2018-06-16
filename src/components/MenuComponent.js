c02c57b9aedfcc7de7b05e66250872ad89fb47fb
Dieu Merci KIMPOLO <dieumerci.k.nkokolo@aims-senegal.org>
2018-06-16 10:33:50 +0000

    update

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -4,14 +4,34 @@ class Menu extends Component{
     constructor(props){
         super(props);
         this.state={
-            
+          selectedDish:null
+        };
+        this.onDishSelect=this.onDishSelect.bind(this);
+        this.renderDish=this.renderDish.bind(this);
+    }
+    onDishSelect(dish){
+        this.setState({selectedDish:dish});
+    }
+    renderDish(dish){
+        if(dish !=null){
+          return (
+              <Card>
+                    <CardImg width="100%" object src={dish.image} alt={dish.name}/>
+              <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+              </CardBody>
+              </Card>
+          )
+        }else{
+            <div></div>
         }
     }
     render(){
         const menu=this.props.dishes.map((dish)=>{
             return(
                 <div key={dish.id} className="col-12  col-md-5 m-1">
-                <Card >
+                <Card onClick={()=>this.onDishSelect(dish)}>
                    <CardImg width="100%" object src={dish.image} alt={dish.name}/>
                  <CardImgOverlay body className="ml-5">
                  <CardTitle>{dish.name}</CardTitle>
@@ -25,6 +45,9 @@ class Menu extends Component{
             <div className="row">
                {menu}
             </div>
+            <div className="row">
+            {this.renderDish(this.state.selectedDish)}
+         </div>
           </div>
         );
     }
